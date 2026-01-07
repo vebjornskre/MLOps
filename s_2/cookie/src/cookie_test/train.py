5ad8b23aa569b4712b794d97d056247737807b7b
Vebjørn Skre <64773925+vebjornskre@users.noreply.github.com>
2026-01-07 16:30:58 +0100

    coockie_template

---
+++ s_2/cookie/src/cookie_test/train.py
@@ -0,0 +1,10 @@
+from cookie_test.model import Model
+from cookie_test.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
